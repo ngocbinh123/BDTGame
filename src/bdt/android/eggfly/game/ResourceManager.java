73783a2eb3e202b8ab9122a8fe5b56b76f96955d
Nguyen Ngoc Binh <MYLOVE@Nguyens-MacBook-Pro.local>
2014-11-16 18:39:39 +0700

    update reroucremanager.java

--- src/bdt/android/eggfly/game/ResourceManager.java
+++ src/bdt/android/eggfly/game/ResourceManager.java
@@ -1,10 +1,38 @@
 package bdt.android.eggfly.game;
 
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+
 /**
  * This ResourceManager adds the ability to use a set of lower-quality textures
  * if desired. It also includes methods for determining an accurate Font texture
  * size to prevent wasting valuable texture memory.
  **/
-public class ResourceManager extends Object{
+public class ResourceManager extends Object {
+	public static ResourceManager INSTANCE = new ResourceManager();
+
+	public Texture mTextureBasket;
+	public TiledTextureRegion mTextureRegionBasket;
+
+	public Texture mTextureEgg;
+	public TiledTextureRegion mTextureRegionEgg;
+	
+	public Texture mTextureBackground;
+	public TiledTextureRegion mTextureRegionBackground;
+
+	// Retrieves a global instance of the ResourceManager
+	public static ResourceManager getInstance() {
+		return INSTANCE;
+	}
+	
+	public ResourceManager() {
+		// TODO Auto-generated constructor stub
+		onLoadGameTexture();
+	}
+
+	public void onLoadGameTexture() {
+		
+
+	}
 
 }
