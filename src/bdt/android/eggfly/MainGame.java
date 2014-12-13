3d73614a3d02848b61610e9dc5c4d7aa4a966763
Nguyen Ngoc Binh <MYLOVE@Nguyens-MacBook-Pro.local>
2014-12-13 15:06:02 +0700

    scrolling vertical background. update background, resourcemanager and maingame.

--- src/bdt/android/eggfly/MainGame.java
+++ src/bdt/android/eggfly/MainGame.java
@@ -6,45 +6,43 @@ import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.scene.background.AutoParallaxBackground;
 import org.anddev.andengine.entity.scene.background.ParallaxBackground.ParallaxEntity;
 import org.anddev.andengine.entity.scene.background.SpriteBackground;
 import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.sprite.TiledSprite;
 import org.anddev.andengine.entity.util.FPSLogger;
-import org.anddev.andengine.opengl.texture.BuildableTexture;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
-import org.anddev.andengine.opengl.vertex.VertexBuffer;
+
 
 import android.util.DisplayMetrics;
 import bdt.android.eggfly.character.Basket;
 import bdt.android.eggfly.character.Eggs;
 import bdt.android.eggfly.game.GameManager;
+import bdt.android.eggfly.game.ResourceManager;
 import bdt.android.eggfly.input.InputTouch;
-
+import bdt.android.eggfly.background.ParallaxBackground2d;
+import bdt.android.eggfly.background.AutoVerticalParallaxBackground;
 public class MainGame extends BaseGameActivity {
 	private static int CAMERA_WIDTH;
 	private static int CAMERA_HEIGHT;
 
 	private Camera mCamera;
 	private Scene scene;
+	
 
 	private Eggs mSpriteEggs;
 	private Basket mSpriteBasket;
-
-	private static MainGame INSTANCE = new MainGame();
-	 
-	private TextureRegion mTextureRegion;
-	private BuildableTexture mBuildableTexture;
-	private Texture mTexture;
-
-	private TextureRegion mParallaxLayerBack;
-    private TextureRegion mParallaxLayerMid;
-    private TextureRegion mParallaxLayerFront;
+	
     
+	private static MainGame INSTANCE = new MainGame();
+
+
 	public static MainGame getInstance() {
 		return INSTANCE;
 	}
@@ -68,42 +66,37 @@ public class MainGame extends BaseGameActivity {
 						CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera)
 				.setNeedsMusic(true).setNeedsSound(true));
 		return engine;
-
 	}
 
 	@Override
 	public void onLoadResources() {
-		// TODO Auto-generated method stub
-		// Load Hình ảnh, âm thanh
-		
-		// Load Background
-		this.mTexture = new Texture(2048,2048,TextureOptions.BILINEAR_PREMULTIPLYALPHA); 
-		this.mTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "tmx/skybg04.png", 0, 0);
-		this.mEngine.getTextureManager().loadTexture(this.mTexture);
-		 
-		// Load Character
 
-		// Load Sound
+		// Load hình ảnh, âm thanh		
+		ResourceManager.INSTANCE.onLoadGameTexture(this);
+		this.mEngine.getTextureManager().loadTexture(ResourceManager.INSTANCE.mTextureBasket);
+		this.mEngine.getTextureManager().loadTexture(ResourceManager.INSTANCE.mTextureEgg);
+		
+		ResourceManager.INSTANCE.onLoadBackgroundTexture(this);
+
+		this.mEngine.getTextureManager().loadTexture(ResourceManager.INSTANCE.mAutoParallaxBackgroundTexture);
+
 	}
 
 	@Override
 	public Scene onLoadScene() {
 		// TODO Auto-generated method stub
-		final Scene scene = new Scene();
-	 	final float centerX = (CAMERA_WIDTH - this.mTextureRegion.getWidth())/2;
-	    final float centerY = (CAMERA_HEIGHT - this.mTextureRegion.getHeight())/2;
-	    
-	    final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(0, 0, 0, 5);
-        autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(0.0f, new Sprite(0, CAMERA_HEIGHT - this.mParallaxLayerBack.getHeight(), this.mParallaxLayerBack)));
-        autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-5.0f, new Sprite(0, 80, this.mParallaxLayerMid)));
-        autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-10.0f, new Sprite(0, CAMERA_HEIGHT - this.mParallaxLayerFront.getHeight(), this.mParallaxLayerFront)));
-	    
-        SpriteBackground bg = new SpriteBackground(new Sprite(0, 0,mTextureRegion));
-	    scene.setBackground(bg);
 		this.mEngine.registerUpdateHandler(new FPSLogger());
+		final Scene scene = new Scene();
 
-		// add background to scene
+		final AutoVerticalParallaxBackground autoParallaxBackground = new AutoVerticalParallaxBackground(0, 0, 0, 200);
+        autoParallaxBackground.addParallaxEntity(new ParallaxBackground2d.ParallaxBackground2dEntity(-0.2f,-0.2f, new Sprite(0, 0, ResourceManager.INSTANCE.mParallaxLayerMid)));
+        scene.setBackground(autoParallaxBackground);
+		
+		final float centerX = (CAMERA_WIDTH - ResourceManager.INSTANCE.mTextureBackground.getWidth())/2;
+	    final float centerY = (CAMERA_HEIGHT - ResourceManager.INSTANCE.mTextureBackground.getHeight())/2;
 
+		//scene.attachChild(sprite);
+		
 		// initial Character
 		// add character to scene
 
--- src/bdt/android/eggfly/game/ResourceManager.java
+++ src/bdt/android/eggfly/game/ResourceManager.java
@@ -1,7 +1,12 @@
 package bdt.android.eggfly.game;
 
 import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+
+import android.content.Context;
 
 /**
  * This ResourceManager adds the ability to use a set of lower-quality textures
@@ -12,13 +17,23 @@ public class ResourceManager extends Object {
 	public static ResourceManager INSTANCE = new ResourceManager();
 
 	public Texture mTextureBasket;
-	public TiledTextureRegion mTextureRegionBasket;
+	public TextureRegion mTextureRegionBasket;
+	public TextureRegion mTextureRegionBasket2;
 
 	public Texture mTextureEgg;
-	public TiledTextureRegion mTextureRegionEgg;
+	public TextureRegion mTextureRegionEgg;
 	
 	public Texture mTextureBackground;
-	public TiledTextureRegion mTextureRegionBackground;
+	public TextureRegion mTextureRegionBackground;
+	
+	public Texture mAutoParallaxBackgroundTexture;
+
+	public TextureRegion mParallaxLayerBack;
+	public TextureRegion mParallaxLayerMid;
+	public TextureRegion mParallaxLayerFront;
+	
+	//public Context pContext;
+	
 
 	// Retrieves a global instance of the ResourceManager
 	public static ResourceManager getInstance() {
@@ -27,12 +42,35 @@ public class ResourceManager extends Object {
 	
 	public ResourceManager() {
 		// TODO Auto-generated constructor stub
-		onLoadGameTexture();
+		//onLoadGameTexture(pContext);
 	}
 
-	public void onLoadGameTexture() {
+	public void onLoadGameTexture(Context pContext) {
+		TextureRegionFactory.setAssetBasePath("tmx/");
 		
-
+		this.mTextureBasket = new Texture(512, 512,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mTextureRegionBasket = TextureRegionFactory.createFromAsset(mTextureBasket, 
+				pContext, "basket02.png", 0, 0);
+		
+		
+		this.mTextureEgg = new Texture(256, 256,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mTextureRegionEgg = TextureRegionFactory.createFromAsset(mTextureEgg, 
+				pContext, "egg.png", 0, 0);
+		this.mAutoParallaxBackgroundTexture = new Texture(2048, 2048,TextureOptions.DEFAULT);
+		this.mParallaxLayerFront =TextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, pContext, "skybg04.png", 0,0);
+	    this.mParallaxLayerBack = TextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture,  pContext, "skybg04.png", 0,188);
+	    this.mParallaxLayerMid = TextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, pContext, "skybg04.png", 0,669);
+	}
+	
+	public void onLoadBackgroundTexture(Context pContext) {
+		TextureRegionFactory.setAssetBasePath("tmx/");
+				
+		this.mTextureBackground = new Texture(2048, 2048,TextureOptions.BILINEAR_PREMULTIPLYALPHA);			
+		
+		this.mTextureRegionBackground = TextureRegionFactory.createFromAsset(mTextureBackground, 
+				pContext, "skybg04.png", 0, 0);
+		
 	}
 
+	
 }
